aafa3dbf25fe04baec5e74ea07c9546480207ce8
MR-GitCode <michael.ring.87@web.de>
2025-02-19 20:42:24 +0100

    js overlay stats

--- script.js
+++ script.js
@@ -50,14 +50,54 @@ function renderTypNav() {
 
 async function loadOverlay(pokeId) {
     document.getElementById('overlay').classList.remove('hidden');
-    // let response = await fetch(POKE_URL + pokeId);
-    // let data= await response.json();
-    // let name = data.species.name
-    // let stats
-    // for (let i = 0; i < array.length; i++) {
-    //     const element = array[i];
-        
-    // }
-    // console.log(stats);
-    // console.log(stats);
+    let overlay = document.getElementById('overlay');
+    let data = await getData(pokeId)
+    overlay.innerHTML = "";
+    overlay.innerHTML = renderOverlay(data); 
+    loadAbout(pokeId)  
+}
+
+
+async function loadAbout(pokeId) {
+    let pokeStats = document.getElementById('stats');
+    pokeStats.innerHTML = "";
+    let data = await getData(pokeId)
+    console.log(data);    
+    pokeStats.innerHTML = renderAbout(data);
+}
+
+async function loadStats(pokeId) {
+    let pokeStats = document.getElementById('stats');
+    pokeStats.innerHTML = "";
+    let data = await getData(pokeId)
+    let statsOfPokemon = data.stats;
+    for (let i = 0; i < statsOfPokemon.length; i++) {
+        let statName = statsOfPokemon[i].stat.name;
+        let statValue = statsOfPokemon[i].base_stat;
+        pokeStats.innerHTML += renderStats(statName, statValue);
+    }
+}
+
+async function loadEvolution(pokeId) {
+    let pokeStats = document.getElementById('stats');
+    pokeStats.innerHTML = "";
+    let data = await getData(pokeId)
+    let statsEvolution = data
+    pokeStats.innerHTML += renderEvolution();
+}
+
+function beforePokemon(pokeId) {
+    let beforePokemon = pokeId -1; //Überlegen was soll passieren bei Id=0 
+    loadOverlay(beforePokemon)
+}
+
+function nextPokemon(pokeId) {
+    let nextPokemon = pokeId +1;
+    loadOverlay(nextPokemon )
+}
+
+async function getData(pokeId) {
+    let response = await fetch(POKE_URL + pokeId);
+    let data= await response.json();
+    return data;
 }
--- scripts/template.js
+++ scripts/template.js
@@ -23,3 +23,55 @@ function renderTypSlots(typName) {
             <img  src="./assets/icons/types/${typName}.svg" alt="${typName}"></img>
             </div>`
 }
+
+function renderOverlay(data) {
+    return ` <div  class="overlay-card">
+            <div class="overlay-header">
+                <p>#${data.id}</p>
+                <h3>${data.species.name}</h3>
+            </div>
+
+            <div class="overlay-img">
+                <img src="${data.sprites.versions['generation-v']['black-white'].animated.front_shiny}">
+                <div>
+                    Type
+                    <!--positon absolute ->container relative   -->
+                </div>
+            </div>
+
+            <div class="overlay-stats">
+                <div id="headline">
+                    <h5 onclick="loadAbout(${data.id})">About</h5>
+                    <h5 onclick="loadStats(${data.id})">Stats</h5>
+                    <h5 onclick="loadEvolution(${data.id})">Evolution</h5>
+                </div>
+
+                <div id="stats"></div>
+            </div>
+
+            <div class="button">
+                <button onclick="beforePokemon(${data.id})">left</button>
+                <button onclick="nextPokemon(${data.id})">right</button>
+            </div>            
+        </div>`
+}
+
+function renderStats(statName, statValue) {
+    return `<div>
+                <p><b>${statName}: </b>${statValue}</p>
+            </div>`
+}
+
+function renderAbout(data) {
+    return `<div>
+                <p><b>Height:</b> ${data.height}</p>
+                <p><b>Weight:</b> ${data.weight}</p>
+                <p><b>Abilities:</b>pfad</p>
+            </div>`
+}
+
+function renderEvolution() {
+    return `<div class="stats">
+                <img src="" alt="">
+            </div>`
+}
